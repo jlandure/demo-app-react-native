2501641cfc3e696b567a157f1bc439562b3c1f09
Julien Landuré <julien.landure@gmail.com>
2020-05-25 07:57:21 +0200

    :bug: correct https://github.com/instamobile/recipes-app-react-native/issues/4 for web error

--- src/navigations/AppNavigation.js
+++ src/navigations/AppNavigation.js
@@ -1,18 +1,18 @@
-import { createAppContainer } from 'react-navigation';
-import {createDrawerNavigator} from 'react-navigation-drawer'
-import {createStackNavigator} from 'react-navigation-stack'
+import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { createStackNavigator } from "react-navigation-stack";
 /* import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
 import {NavigationContainer} from '@react-navigation/native'
 import {createDrawerNavigator} from '@react-navigation/drawer' */
-import HomeScreen from '../screens/Home/HomeScreen';
-import CategoriesScreen from '../screens/Categories/CategoriesScreen';
-import RecipeScreen from '../screens/Recipe/RecipeScreen';
-import RecipesListScreen from '../screens/RecipesList/RecipesListScreen';
-import DrawerContainer from '../screens/DrawerContainer/DrawerContainer';
-import IngredientScreen from '../screens/Ingredient/IngredientScreen';
-import SearchScreen from '../screens/Search/SearchScreen';
-import IngredientsDetailsScreen from '../screens/IngredientsDetails/IngredientsDetailsScreen';
+import HomeScreen from "../screens/Home/HomeScreen";
+import CategoriesScreen from "../screens/Categories/CategoriesScreen";
+import RecipeScreen from "../screens/Recipe/RecipeScreen";
+import RecipesListScreen from "../screens/RecipesList/RecipesListScreen";
+import DrawerContainer from "../screens/DrawerContainer/DrawerContainer";
+import IngredientScreen from "../screens/Ingredient/IngredientScreen";
+import SearchScreen from "../screens/Search/SearchScreen";
+import IngredientsDetailsScreen from "../screens/IngredientsDetails/IngredientsDetailsScreen";
 
 /* const Stack = createStackNavigator();
 
@@ -47,21 +47,21 @@ const MainNavigator = createStackNavigator(
     RecipesList: RecipesListScreen,
     Ingredient: IngredientScreen,
     Search: SearchScreen,
-    IngredientsDetails: IngredientsDetailsScreen
+    IngredientsDetails: IngredientsDetailsScreen,
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: "Home",
     // headerMode: 'float',
     defaulfNavigationOptions: ({ navigation }) => ({
       headerTitleStyle: {
-        fontWeight: 'bold',
-        textAlign: 'center',
-        alignSelf: 'center',
+        fontWeight: "bold",
+        textAlign: "center",
+        alignSelf: "center",
         flex: 1,
-      }
-    })
+      },
+    }),
   }
-); 
+);
 
 /* const Drawer = createDrawerNavigator();
 
@@ -82,13 +82,13 @@ function DrawerStack() {
 
 const DrawerStack = createDrawerNavigator(
   {
-    Main: MainNavigator
+    Main: MainNavigator,
   },
   {
-    drawerPosition: 'left',
-    initialRouteName: 'Main',
+    drawerPosition: "left",
+    initialRouteName: "Main",
     drawerWidth: 250,
-    contentComponent: DrawerContainer
+    contentComponent: DrawerContainer,
   }
 );
 
@@ -99,7 +99,9 @@ const DrawerStack = createDrawerNavigator(
     </NavigationContainer>
   )
 } */
- 
-export default AppContainer = createAppContainer(DrawerStack);
+
+//export default AppContainer = createAppContainer(DrawerStack);
+const AppContainer = createAppContainer(DrawerStack);
+export default AppContainer;
 
 console.disableYellowBox = true;
